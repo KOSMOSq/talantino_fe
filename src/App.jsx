396616cf7abb58a011522c97b67f5f47e8a975c4
KOSMOSq <maks2014216@gmail.com>
2023-03-21 11:30:05 +0200

    added file structure and environment variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">talantino</div>;
+	return <div className="App">TALANTINO {process.env.REACT_APP_VERSION}</div>;
 }
 
 export default App;
