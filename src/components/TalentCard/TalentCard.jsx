39d2b5e97b80f4ddea97b71d0d586b5242d4e4d8
Bohdan <rogozyanskiy.bogdan@gmail.com>
2023-03-23 16:49:40 +0200

    started to make talentCard responsive

--- src/components/TalentCard/TalentCard.jsx
+++ src/components/TalentCard/TalentCard.jsx
@@ -14,26 +14,67 @@ const TalentCard = ({ name, surname, profilePicture, kindOfTalent, id, isAuth })
 
     return (
         <>
-            <Card sx={{ width:300, height: 165, borderRadius: "12px" }} title={isAuth ? `${name} ${surname}` : "You need to log in to see talent profiles"}>
-                <CardActionArea onClick={handleClick} >
+            <Card sx={{
+                width: {
+                    xs: 300, //0
+                    sm: 300, //600
+                    md: 300, //900
+                    lg: 300, //1200
+                    xl: 300, //1536
+                },
+                height: {
+                    xs: 165, //0
+                    sm: 165, //600
+                    md: 165, //900
+                    lg: 165, //1200
+                    xl: 165, //1536
+                }, borderRadius: "12px"
+            }} title={isAuth ? `${name} ${surname}` : "You need to log in to see talent profiles"}>
+                <CardActionArea
+                    sx={{
+                        display: "flex",
+                        flexDirection: "row",
+                        height: "inherit",
+                        width: "inherit"
+                    }}
+                    onClick={handleClick} >
+                    
                     <CardMedia
-                        component="img"
-                        height="95"
-                        alt="Talent Avatar"
-                        image={profilePicture ? profilePicture : "errorTrigger"}
-                        onError={({ currentTarget }) => {
-                            currentTarget.onerror = null;
-                            currentTarget.src = noPictureFallback;
-                        }}
+                    sx={{
+                        objectFit: "cover",
+                        height: "inherit",
+                        width: "60%",
+                    
+                    }}
+                    component="img"
+                    
+                    // height={{
+                    //     xs: 0, //0
+                    //     sm: 80, //600
+                    //     md: 20, //900
+                    //     lg: 300, //1200
+                    //     xl: 300, //1536
+                    // }}
+                    alt="Talent Avatar"
+                    image={profilePicture ? profilePicture : "errorTrigger"}
+                    onError={({ currentTarget }) => {
+                        currentTarget.onerror = null;
+                        currentTarget.src = noPictureFallback;
+                    }}
                     />
+                    
+                    
                     <CardContent sx={{
-                        padding: "8px",
+                        
                         display: "flex", 
                         flexDirection: "column",
-                        alignItems: "center"
+                        alignItems: "center",
+                        width: "30%",
+                        textAlign: "center",
                     }}>
-                        <Typography variant="h6" component="div" sx={{ fontSize: "16px", fontWeight: "bold" }}>
-                            {name} {surname}
+                                                
+                        <Typography variant="h6" component="h6" sx={{ fontSize: "16px", fontWeight: "bold"}}>
+                            {name} {"Рогозянськsgsgйблала"}
                         </Typography>
                         <Chip label={kindOfTalent} color="primary" size="small" />
                     </CardContent>
