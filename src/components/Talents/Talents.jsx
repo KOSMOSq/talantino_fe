935a95c09634ebe4c9a3c182ee9dd48ea69dd5ff
Dimas <dimakataevse@gmail.com>
2023-03-24 11:35:32 +0200

    minor

--- src/components/Talents/Talents.jsx
+++ src/components/Talents/Talents.jsx
@@ -1,18 +1,15 @@
-import { Box, Container, Grid, Pagination } from "@mui/material";
 import React from "react";
-import { ChangeViewButton } from "../ChangeViewButton";
-import { testTalents } from "../../common/common";
 import { TalentCard } from "../TalentCard";
+import { ChangeViewButton } from "../ChangeViewButton";
+import { Box, Container, Grid, Pagination } from "@mui/material";
 
-function Talents() {
-
+const Talents = () => {
 	return (
 		<Container sx={{ maxWidth: { xl: "1900px" } }}>
 			<Box zIndex={1} position={"absolute"} right={80} mt={"15px"} mb={"15px"}>
 				{/* handleChange will change view of talets */}
 				<ChangeViewButton handleChange={() => 1}/>
 			</Box>
-
 			<Box display="flex" justifyContent="center" pt={4}>
 				<Grid container item
 					sm={12}
@@ -21,23 +18,20 @@ function Talents() {
 					xl={12}
 					spacing={2}
 					display="flex"
-					justifyContent="center">
-					
-					{/* testTalents will become a state that will contain an elements from server */}
-					{testTalents.map((element, index) => {
+					justifyContent="center"
+				>
+					{talents.map(element => {
 						return (
-							<Grid item key = {element.id}>
+							<Grid item key={element.id}>
 								<TalentCard {...element} />
 							</Grid>
-						)
+						);
 					})}
 				</Grid>
 			</Box>
-			<Pagination sx={{
-				marginTop:"40px"
-			}} count={125}></Pagination>
+			<Pagination sx={{ marginTop: "20px" }} count={125}></Pagination>
 		</Container>
-	)
-}
+	);
+};
 
 export { Talents };
