16a6142f08df8c06ca0757712367b52f54516510
Bohdan <rogozyanskiy.bogdan@gmail.com>
2023-03-27 22:50:02 +0300

    added close btn on the talent profile. Implement redux store with several states. Added saving current page and clicked id for future use in redirecting after login.

---
+++ src/redux/reducers/talentsReducer.js
@@ -0,0 +1,45 @@
+const SET_CURRENT_PAGE = 'talents/SET-CURRENT-PAGE';
+const SET_TOTAL_PAGES = 'talents/SET-TOTAL-PAGES';
+const SET_TALENTS = 'talents/SET-TALENTS';
+const SET_CLICKED_ID = 'talents/SET-CLICKED-ID';
+
+const initialState = {
+    currentPage: 1,
+    totalPages: 1,
+    talents: {},
+    clickedId: null
+}
+
+const talentsReducer = (state = initialState, action) =>{
+    switch (action.type) {
+        case SET_CURRENT_PAGE:
+            return {
+                ...state,
+                currentPage: action.page
+            }
+        case SET_TOTAL_PAGES:
+            return {
+                ...state,
+                totalPages: action.amount
+            }
+        case SET_TALENTS:
+            return {
+                ...state,
+                talents: action.talents
+            }
+        case SET_CLICKED_ID:
+            return {
+                ...state,
+                clickedId: action.clickedId
+            }
+        default:
+            return state;
+    }
+}
+
+export const setCurrentPage = (page) => ({type: SET_CURRENT_PAGE, page});
+export const setTotalPages = (amount) => ({type: SET_TOTAL_PAGES, amount});
+export const setTalents = (talents) => ({type: SET_TALENTS, talents});
+export const setClikedId= (clickedId) => ({type: SET_CLICKED_ID, clickedId});
+
+export default talentsReducer;
