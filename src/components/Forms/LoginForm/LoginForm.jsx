685ca1dd159885282fe84b25c8559d420bd1d44a
KOSMOSq <maks2014216@gmail.com>
2023-03-28 12:12:03 +0300

    add name under avatar

--- src/components/Forms/LoginForm/LoginForm.jsx
+++ src/components/Forms/LoginForm/LoginForm.jsx
@@ -1,10 +1,11 @@
 import { useForm } from "react-hook-form";
 import { TextField, Button, Typography, Container } from "@mui/material";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { authAPI } from "../../../api/authAPI";
 import { mailValidation } from "../validation";
-import { useDispatch } from "react-redux";
-import { setToken } from "../../../redux/reducers/authReducer";
+import { useDispatch, useSelector } from "react-redux";
+import { setTalentData, setToken } from "../../../redux/reducers/authReducer";
+import { useEffect } from "react";
 
 function LoginForm() {
 	const {
@@ -13,8 +14,15 @@ function LoginForm() {
 		reset,
 		formState: { errors, isValid },
 	} = useForm({ mode: "onTouched" });
-
 	const dispatch = useDispatch();
+	const isAuth = useSelector((store) => store.auth.isAuth);
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		if (isAuth) {
+			navigate(`/talents`);
+		}
+	}, [isAuth, navigate]);
 
 	const onSubmit = async (data) => {
 		const response = await authAPI.login({
@@ -23,6 +31,15 @@ function LoginForm() {
 		});
 		localStorage.setItem("token", response.token);
 		dispatch(setToken(response.token));
+		const talentResponse = await authAPI.auth(response.token);
+		dispatch(
+			setTalentData(
+				response.id,
+				response.name,
+				response.surname,
+				response.avatar
+			)
+		);
 		reset();
 	};
 
@@ -53,14 +70,14 @@ function LoginForm() {
 						type="password"
 						{...register("password", {
 							required: "Password is required",
-							// minLength: {
-							// 	value: 8,
-							// 	message: "Password should be at least 8 characters",
-							// },
-							// pattern: {
-							// 	value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d\-_@$!%*#?&]{8,}$/,
-							// 	message: "Your pass doesn't meet requirments",
-							// },
+							minLength: {
+								value: 8,
+								message: "Password should be at least 8 characters",
+							},
+							pattern: {
+								value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d\-_@$!%*#?&]{8,}$/,
+								message: "Your pass doesn't meet requirments",
+							},
 						})}
 						error={Boolean(errors.password)}
 						helperText={
