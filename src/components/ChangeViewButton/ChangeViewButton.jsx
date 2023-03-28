19eb44a2dd3c5c466accf659096efef259d76e1e
Bohdan <rogozyanskiy.bogdan@gmail.com>
2023-03-28 16:05:09 +0300

    added fancy change view button

--- src/components/ChangeViewButton/ChangeViewButton.jsx
+++ src/components/ChangeViewButton/ChangeViewButton.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react';
 import styles from './ChangeViewButton.module.css'
 import { ReactComponent as StyleOfViewGrid } from '../../assets/styleOfViewGrid.svg'
 import { ReactComponent as StyleOfViewList } from '../../assets/styleOfViewList.svg'
+import { ToggleButton, ToggleButtonGroup } from '@mui/material';
+import ViewModuleIcon from '@mui/icons-material/ViewModule';
+import ViewListIcon from '@mui/icons-material/ViewList';
 
 function ChangeViewButton({handleChange}) {
   const [isGrid, setIsGrid] = useState(true);
@@ -24,23 +27,27 @@ function ChangeViewButton({handleChange}) {
   }
 
   return (
-    <div className={styles['container']}>
-      <div onClick={handleClick}>
-        <StyleOfViewGrid
-        className={styles['button']}
-        style = {isGrid ? styleViewActive : styleViewInActive}
-        data-type-view = "grid"
-      />
-      </div>
+    <ToggleButtonGroup value={"grid"}>
+      <ToggleButton value = "grid"> <ViewModuleIcon/></ToggleButton>
+      <ToggleButton value = "list"> <ViewListIcon/></ToggleButton>
+    </ToggleButtonGroup>
+    // <div className={styles['container']}>
+    //   <div onClick={handleClick}>
+    //     <StyleOfViewGrid
+    //     className={styles['button']}
+    //     style = {isGrid ? styleViewActive : styleViewInActive}
+    //     data-type-view = "grid"
+    //   />
+    //   </div>
       
-      <div onClick={handleClick}>
-        <StyleOfViewList
-          className={styles['button']}
-          style = {isGrid ? styleViewInActive : styleViewActive}
-          data-type-view = "list"
-        />
-      </div>
-    </div>
+    //   <div onClick={handleClick}>
+    //     <StyleOfViewList
+    //       className={styles['button']}
+    //       style = {isGrid ? styleViewInActive : styleViewActive}
+    //       data-type-view = "list"
+    //     />
+    //   </div>
+    // </div>
   )
 }
 
