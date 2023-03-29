9fa4e7c74b36aca254036ba4a3e189f7eab00993
Dimas <dimakataevse@gmail.com>
2023-03-29 16:17:14 +0300

    last work

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -47,12 +47,17 @@ const Settings = () => {
             avatar: avatar,
             experience: experience,
             location: location,
-            links: links,
+            links: {
+                zero: links[0],
+                one: links[1],
+                two: links[2],
+                three: links[3],
+            },
         }
     });
 
     const onSubmit = async (data) => {
-        data.links = data.links.split(",");
+        console.log(data.links = [data.links.zero, data.links.one, data.links.two, data.links.three]);
         const response = await talentsAPI.changeData(id, token, data);
         const responseAuth = await authAPI.auth(token);
         dispatch(setTalentData(responseAuth));
@@ -61,91 +66,159 @@ const Settings = () => {
 
     return (
         <>
-            <Container sx={{marginTop: 4, paddingBottom: 5}}>
-                
+            <Container sx={{ marginTop: 4, paddingBottom: 5 }}>
+
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <Box display={"flex"} width={"100%"} justifyContent={"space-between"}>
-                        <Typography 
+                        <Typography
                             sx={{ display: 'inline', fontSize: 24, fontWeight: "bold" }}
                             component="h6"
-                            variant="h6"> 
-                            Editing profile 
+                            variant="h6">
+                            Editing profile
                         </Typography>
-                        <Button variant="outlined" type="submit" sx={{fontSize: 16}}>
+                        <Button variant="outlined" type="submit" sx={{ fontSize: 16 }}>
                             Save changes
                         </Button>
                     </Box>
-                    <Divider sx={{marginTop:2}}/>
-                    
-                    <Box display={"flex"} justifyContent={"space-between"} mt={2}> 
-                        
+                    <Divider sx={{ marginTop: 2 }} />
+
+                    <Box display={"flex"} justifyContent={"space-between"} mt={2}>
+
                         <Box width={{
                             lg: "55vw",
                             xl: "50vw"
-                            }}>
+                        }}>
                             <TextField label="Name" {...register("name", {
-                                required: "This field is required",
-                                
+                                required: "First name is required",
+                                maxLength: {
+                                    value: 24,
+                                    message: "Your name is too long"
+                                },
+                                minLength: {
+                                    value: 2,
+                                    message: "Your name is too short"
+                                },
+                                pattern: {
+                                    value: /^[a-zA-Z]+$/,
+                                    message: "First name can only contain letters",
+                                },
                             })} />
-                            <TextField label="Surname" sx={{marginLeft: 2}} {...register("surname", {
-                                required: "This field is required",
+                            <TextField label="Surname" sx={{ marginLeft: 2 }} {...register("surname", {
+                                required: "Last name is required",
+                                maxLength: {
+                                    value: 24,
+                                    message: "Your surname is too long"
+                                },
+                                minLength: {
+                                    value: 2,
+                                    message: "Your surname is too short"
+                                },
+                                pattern: {
+                                    value: /^[a-zA-Z]+$/,
+                                    message: "Last name can only contain letters",
+                                },
                             })} />
-                            <TextField label="Location" sx={{marginLeft: 2}} {...register("location", {
-
+                            <TextField label="Location" sx={{ marginLeft: 2 }} {...register("location", {
+                                maxLength: {
+                                    value: 100,
+                                    message: "Your location is to long"
+                                },
+                                minLength: {
+                                    value: 2,
+                                    message: "Your location is to short"
+                                },
+                                pattern: {
+                                    value: /^[a-zA-Z]+$/,
+                                    message: "Location can only contain letters",
+                                }
                             })} />
-                            
-                            <TextField 
-                                label="Description" 
-                                multiline 
-                                maxRows ={7} 
-                                minRows = {7}
-                                sx={{width: "91%", marginTop: 2}}
+                            <TextField
+                                label="Description"
+                                multiline
+                                maxRows={7}
+                                minRows={7}
+                                sx={{ width: "91%", marginTop: 2 }}
                                 {...register("description", {
-
-                            })} />
+                                    maxLength: {
+                                        value: 3000,
+                                        message: "Your description is to long"
+                                    },
+                                    minLength: {
+                                        value: 2,
+                                        message: "Your description is to short"
+                                    },
+                                })} />
                             <Box mt={2}>
                                 <TextField label="Kind of talent" {...register("kind", {
-                                    required: "This field is required",
+                                    required: "Kind of talent is required",
+                                    maxLength: {
+                                        value: 18,
+                                        message: "Your talent is to BIG"
+                                    },
+                                    minLength: {
+                                        value: 2,
+                                        message: "Your talent is to short"
+                                    },
+                                    pattern: {
+                                        value: /^[a-zA-Z ]+$/,
+                                        message: "Kind of talent can only contain letters",
+                                    },
                                 })} />
-                                <TextField label="Experience" sx={{marginLeft: 2}} {...register("experience", {
-
+                                <TextField label="Experience" sx={{ marginLeft: 2 }} {...register("experience", {
+                                    validate: value => (Number(value) < 60 && Number(value) > 1) || "You have too much experience XD",
+                                    pattern: {
+                                        value: /^[0-9]+$/,
+                                        message: "Experience can only contain letters",
+                                    },
                                 })} />
                             </Box>
 
                             <Box display={"flex"} flexDirection={"row"} mt={10}>
-                                <TextField label="Social link" {...register("links", {
-
+                                <TextField label="Social link" {...register("links.zero", {
+                                    maxLength: {
+                                        value: 100,
+                                        message: "Too long link"
+                                    }
                                 })} />
-                                <TextField label="Social link" sx={{marginLeft: 2}} {...register("links", {
-
+                                <TextField label="Social link" sx={{ marginLeft: 2 }} {...register("links.one", {
+                                    maxLength: {
+                                        value: 100,
+                                        message: "Too long link"
+                                    }
                                 })} />
-                                <TextField label="Social link" sx={{marginLeft: 2}} {...register("links", {
-
+                                <TextField label="Social link" sx={{ marginLeft: 2 }} {...register("links.two", {
+                                    maxLength: {
+                                        value: 100,
+                                        message: "Too long link"
+                                    }
                                 })} />
-                                <TextField label="Social link" sx={{marginLeft: 2}} {...register("links", {
-
+                                <TextField label="Social link" sx={{ marginLeft: 2 }} {...register("links.three", {
+                                    maxLength: {
+                                        value: 100,
+                                        message: "Too long link"
+                                    }
                                 })} />
-                            </Box>    
-                            
+                            </Box>
+
                         </Box>
 
                         <Box display={"flex"} flexDirection={"column"} justifyContent={"space-between"} mr={2}>
                             <Box display={"flex"} flexDirection={"column"} alignItems={"center"}>
                                 <Avatar
-                                alt={name}
-                                src={prPicture || avatar || "error"}
-                                sx={{ width: 190, height: 190, marginBottom: 2, fontSize: "64px" }}
+                                    alt={name}
+                                    src={prPicture || avatar || "error"}
+                                    sx={{ width: 190, height: 190, marginBottom: 2, fontSize: "64px" }}
                                 />
 
-                                <TextField  label="Avatar URL" {...register("avatar", {
-            
-                                })} onChange={handleChangeAvatar}/> 
-                            </Box>   
+                                <TextField label="Avatar URL" {...register("avatar", {
+
+                                })} onChange={handleChangeAvatar} />
+                            </Box>
                             <Box display={"flex"} justifyContent={"center"} mb={1}>
-                                <DeleteTalent/>  
-                            </Box>    
+                                <DeleteTalent />
+                            </Box>
                         </Box>
-                        
+
                     </Box>
                 </form>
             </Container>
