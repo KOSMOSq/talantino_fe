5566abaf16e072209536391a722d4eff506ee814
Bohdan <rogozyanskiy.bogdan@gmail.com>
2023-04-05 19:57:20 +0300

    implemented api to get talent proofs

--- src/components/TalentProfile/components/MainContent/components/TalentProofArea/TalentProofArea.jsx
+++ src/components/TalentProfile/components/MainContent/components/TalentProofArea/TalentProofArea.jsx
@@ -1,8 +1,33 @@
-import { Box, Grid, Typography } from "@mui/material";
-import { proofs } from "../../../../../../common/proofs";
+import { Box, Grid, LinearProgress, Typography } from "@mui/material";
+//import { proofs } from "../../../../../../common/proofs";
 import { TalentProof } from "./components/TalentProof";
+import { proofsAPI } from "../../../../../../api/proofsAPI";
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 
 function TalentProofArea() {
+	const [proofs, setProofs] = useState();
+	const [isLoading, setIsLoading] = useState(false);
+	const token = useSelector(store => store.auth.token);
+	const id = useSelector(store => store.auth.id);
+	const {talentId} = useParams();
+	const status = id === Number(talentId) ? "ALL" : "PUBLISHED";
+	
+	useEffect(()=>{
+		const getProofs = async () =>{
+			setIsLoading(true);
+			const response = await proofsAPI.getTalentProofs(token, talentId, undefined, status);
+			setProofs(response.proofs);
+			setIsLoading(false);
+		}
+		getProofs().catch((error) => console.log(error));
+	},[])
+
+	if (isLoading || !proofs) {
+		return <LinearProgress />;
+	}
+
 	return (
 		<>
 			<Box>
