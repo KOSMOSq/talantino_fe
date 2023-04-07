a82c6ad0ef68dca23de27d7b2c2cd6f5199c309c
Bohdan <rogozyanskiy.bogdan@gmail.com>
2023-04-07 17:43:05 +0300

    intermediate commit

--- src/components/Proofs/Proofs.jsx
+++ src/components/Proofs/Proofs.jsx
@@ -5,8 +5,8 @@ const Proofs = () => {
     return (
         <Container  sx={{ width: "700px", display: "flex", flexDirection: "column" }}>
             <ProofsArea />
-            <Pagination
-                sx={{ marginTop: "20px", marginLeft: "auto", marginRight: "auto", marginBottom: 2 }}/>
+            {/* <Pagination
+                sx={{ marginTop: "20px", marginLeft: "auto", marginRight: "auto", marginBottom: 2 }}/> */}
         </Container>
     );
 };
