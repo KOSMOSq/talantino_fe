d43e58bc14a1ef75f12569e418d68bd863160cfd
Bohdan <rogozyanskiy.bogdan@gmail.com>
2023-04-10 21:56:38 +0300

    added pagination for proofs

--- src/components/Proofs/Proofs.jsx
+++ src/components/Proofs/Proofs.jsx
@@ -2,23 +2,34 @@ import { Container, LinearProgress, Pagination } from "@mui/material";
 import { ProofsArea } from "./components/ProofsArea/ProofsArea";
 import { useEffect, useState } from "react";
 import { proofsAPI } from "../../api/proofsAPI";
-import { useLocation } from "react-router";
+import { useNavigate } from "react-router";
+import { useSearchParams } from "react-router-dom";
 
 const Proofs = () => {
     const [proofs, setProofs] = useState();
     const [isLoading, setIsLoading] = useState(true);
-    const [page, setPage] = useState(0);
+    const [page, setPage] = useState(1);
     const [totalPages, setTotalPages] = useState();
+    const [searchParams] = useSearchParams();
+    const navigate = useNavigate();
 
-    const getProofs = async () => {
+    const getProofs = async (page, count) => {
         const response = await proofsAPI.getProofs(
             undefined,
             undefined,
-            page,
-            undefined
+            page - 1,
+            count
         );
+        const total = Math.ceil(response.totalAmount / count);
+        setTotalPages(total);
+
+        if (page > total) {
+            setPage(total);
+            navigate(`/proofs?page=${total}`);
+            return;
+        }
+
         setProofs(response.proofs);
-        setTotalPages(response.totalAmount);
         setIsLoading(false);
     };
 
@@ -26,20 +37,42 @@ const Proofs = () => {
         if (!isLoading) {
             setIsLoading(true);
         }
-        getProofs().catch(err => console.log(err));
-    }, []);
+        const urlPage = Number(searchParams.get("page")) || 1;
+        setPage(urlPage);
+        if (page < 1) {
+            setPage(1);
+            navigate(`/proofs?page=1`);
+            return;
+        }
+
+        getProofs(page, 9).catch(err => console.log(err));
+    }, [page]);
 
     if (isLoading) {
         return <LinearProgress />;
     }
+
+    const handleChange = (e, value) => {
+        setPage(value);
+        navigate(`/proofs?page=${value}`);
+    };
 
     return (
         <Container
             sx={{ width: "700px", display: "flex", flexDirection: "column" }}
         >
             <ProofsArea proofs={proofs} />
-            {/* <Pagination
-                sx={{ marginTop: "20px", marginLeft: "auto", marginRight: "auto", marginBottom: 2 }}/> */}
+            <Pagination
+                sx={{
+                    marginTop: 1,
+                    marginLeft: "auto",
+                    marginRight: "auto",
+                    marginBottom: 4
+                }}
+                page={page}
+                count={totalPages}
+                onChange={handleChange}
+            />
         </Container>
     );
 };
