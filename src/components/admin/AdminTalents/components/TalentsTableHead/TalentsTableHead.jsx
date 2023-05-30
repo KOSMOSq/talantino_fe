c8d039e8d9a1e78e2a687c2b47419241a7f847de
Bohdan <rogozyanskiy.bogdan@gmail.com>
2023-05-30 21:52:09 +0300

    added ability to unblock talents

--- src/components/admin/AdminTalents/components/TalentsTableHead/TalentsTableHead.jsx
+++ src/components/admin/AdminTalents/components/TalentsTableHead/TalentsTableHead.jsx
@@ -4,17 +4,18 @@ const TalentsTableHead = () => {
     return (
         <TableHead>
             <TableRow>
-                <TableCell>Action</TableCell>
+                <TableCell>Delete&nbsp;user</TableCell>
+                <TableCell align="right">Unblock</TableCell>
                 <TableCell align="right">id</TableCell>
                 <TableCell align="right">Account&nbsp;Status</TableCell>
                 <TableCell align="right">Name</TableCell>
                 <TableCell align="right">Surname</TableCell>
                 <TableCell align="right">Email</TableCell>
                 <TableCell align="right">Kind&nbsp;of&nbsp;talent</TableCell>
+                <TableCell align="right">DeletionDate</TableCell>
                 <TableCell align="right">
                     Verification&nbsp;ExpireDate
                 </TableCell>
-                <TableCell align="right">DeletionDate</TableCell>
             </TableRow>
         </TableHead>
     );
