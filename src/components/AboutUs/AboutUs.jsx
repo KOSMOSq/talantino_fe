34e3064d81a27e09cf6b20743eae44144bbc3e23
KOSMOSq <maks2014216@gmail.com>
2023-05-31 14:17:06 +0300

    about

---
+++ src/components/AboutUs/AboutUs.jsx
@@ -0,0 +1,12 @@
+import { Footer } from "../../shared/components/Footer/Footer";
+
+const AboutUs = () => {
+    return (
+        <>
+            <h1>About Us</h1>
+            <Footer />
+        </>
+    );
+};
+
+export { AboutUs };
