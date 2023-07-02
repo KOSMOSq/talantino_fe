4ba894c2cab05841fad4578fac8a04eb1c410b90
Bohdan <rogozyanskiy.bogdan@gmail.com>
2023-07-02 13:29:52 +0300

    fixed version of typescript for cra. deleted '.ts' in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import { PopUpMessage } from "./components/PopUpMessage/PopUpMessage.jsx";
 import { ThemeProvider } from "@mui/material/styles";
 import { fontFamilyTheme } from "./shared/themes/fontFamilyTheme.js";
 import { ModalConfirmation } from "./components/ModalConfirmation/ModalConfirmation.jsx";
-import { closeModal } from "./redux/reducers/appReducer.ts";
+import { closeModal } from "./redux/reducers/appReducer";
 
 function App() {
     const isInitialized = useSelector(store => store.auth.isInitialized);
